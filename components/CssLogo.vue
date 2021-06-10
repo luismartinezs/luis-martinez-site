b1b30e9f759198dded52f53adc84f7de14f024a7
luismartinezs <martinez.s.luis@gmail.com>
2021-06-10 23:31:55 +0200

    UNselectable logo and dynamic style of menu links

--- components/CssLogo.vue
+++ components/CssLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative w-12 h-12 rounded bg-gradient-to-br from-indigo-500 to-indigo-700"
+    class="relative w-12 h-12 rounded select-none bg-gradient-to-br from-indigo-500 to-indigo-700"
   >
     <div
       class="absolute top-0 right-0 flex items-center justify-center w-10 h-10 border-t-0 border-b-4 border-l-4 border-r-0 border-white"
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -5,11 +5,22 @@
       <CssLogo />
     </NuxtLink>
     <nav class="flex justify-end space-x-8">
-      <NuxtLink to="/" class="text-gray-700 link link-primary">Home</NuxtLink>
-      <NuxtLink to="/about" class="text-gray-700 link link-primary"
+      <NuxtLink
+        to="/"
+        class="text-gray-700 link link-primary"
+        :class="[$route.name === 'index' ? 'underline' : '']"
+        >Home</NuxtLink
+      >
+      <NuxtLink
+        to="/about"
+        class="text-gray-700 link link-primary"
+        :class="[$route.name === 'About' ? 'underline' : '']"
         >About</NuxtLink
       >
-      <NuxtLink to="/resume" class="text-gray-700 link link-primary"
+      <NuxtLink
+        to="/resume"
+        class="text-gray-700 link link-primary"
+        :class="[$route.name === 'Resume' ? 'underline' : '']"
         >Resume</NuxtLink
       >
     </nav>
