fa70dcf465a1fbe84ff278d4c8e6521fd30e91bf
luismartinezs <martinez.s.luis@gmail.com>
2021-06-12 13:18:06 +0200

    Try new logos

---
+++ components/CssLogoJsStyle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex space-x-1">
+    <div
+      class="relative w-12 h-12 select-none bg-gradient-to-br from-indigo-500 to-indigo-700"
+    >
+      <span class="absolute bottom-0 right-0 pr-1 text-xl font-bold text-white"
+        >LM</span
+      >
+    </div>
+    <div
+      class="relative w-12 h-12 select-none bg-gradient-to-br from-indigo-200 to-indigo-400"
+    >
+      <span
+        class="absolute bottom-0 right-0 pr-1 text-xl font-bold text-gray-700"
+        >LM</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CssLogoJsStyle',
+}
+</script>
+
+<style scoped></style>
---
+++ pages/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CssLogoJsStyle />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
